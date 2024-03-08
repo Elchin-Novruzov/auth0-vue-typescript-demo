affd04f0de28ccf18caeae7a1625078f3bd19331
Elchin-Novruzov <88887189+Elchin-Novruzov@users.noreply.github.com>
2024-03-08 05:23:31 +0100

    https://github.com/Elchin-Novruzov/auth0-vue-typescript-demo

---
+++ node_modules/@auth0/auth0-vue/src/global.ts
@@ -0,0 +1,19 @@
+export { Auth0Plugin } from './plugin';
+export * from './interfaces';
+export * from './guard';
+
+export {
+  AuthorizationParams,
+  PopupLoginOptions,
+  PopupConfigOptions,
+  GetTokenWithPopupOptions,
+  LogoutUrlOptions,
+  CacheLocation,
+  GetTokenSilentlyOptions,
+  IdToken,
+  User,
+  ICache,
+  InMemoryCache,
+  LocalStorageCache,
+  Cacheable,
+} from '@auth0/auth0-spa-js';
